a8411b2f9f89787303280c43d4b09e4c20ab40e3
Guilherme Covaleki <gcovaleki@gmail.com>
2025-06-15 17:13:18 -0300

    Update desfragmentacao_disco.py

--- desfragmentacao_disco.py
+++ desfragmentacao_disco.py
@@ -18,14 +18,35 @@ def fragmentar_disco(disco_virtual, disco_cheio_evento):
             disco_cheio_evento.set()
             break
 
+def desfragmentar_disco(disco_virtual, disco_cheio_evento, desfragmentado_evento):
+    while True:
+        time.sleep(5)
+        dados = [b for b in disco_virtual if b is not None]
+
+        for i in range(len(disco_virtual)):
+            disco_virtual[i] = dados[i] if i < len(dados) else None
+
+        print("[Desfragmentador] Disco desfragmentado.")
+        print("Estado atual:", list(disco_virtual))
+
+        if disco_cheio_evento.is_set() and all(b is not None for b in disco_virtual):
+            print("[Desfragmentador] Finalizado. Disco 100% cheio e desfragmentado.\n")
+            desfragmentado_evento.set()
+            break
+
 if _name_ == "_main_":
     multiprocessing.freeze_support()
     manager = multiprocessing.Manager()
     disco_virtual = manager.list([None] * 20)
+
     disco_cheio_evento = multiprocessing.Event()
+    desfragmentado_evento = multiprocessing.Event()
 
     fragmentador = threading.Thread(target=fragmentar_disco, args=(disco_virtual, disco_cheio_evento))
-    fragmentador.start()
-    fragmentador.join()
+    desfragmentador = threading.Thread(target=desfragmentar_disco, args=(disco_virtual, disco_cheio_evento, desfragmentado_evento))
 
-    print("Estado final do disco:", list(disco_virtual))
+    fragmentador.start()
+    desfragmentador.start()
+
+    desfragmentado_evento.wait()
+    print("Simulação concluída.")
